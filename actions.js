894a617299d78bb9f68383a17d15d995d2709b3c
Brian Broll <brian.broll@vanderbilt.edu>
2017-02-07 11:48:11 -0600

    Update the cslot size on animated removeBlock. Fixes #228

--- actions.js
+++ actions.js
@@ -1578,6 +1578,7 @@ ActionManager.prototype._onRemoveBlock = function(id, userDestroy, callback) {
         parent = block.parent,
         afterRemove = function() {
             block[method]();
+
             myself.__updateBlockDefinitions(block);
             callback();
         };
@@ -1620,15 +1621,14 @@ ActionManager.prototype._onRemoveBlock = function(id, userDestroy, callback) {
             );
         } else {
             afterRemove();
-
-            // Update parent block's UI
-            if (parent) {
-                if (parent.reactToGrabOf) {
-                    parent.reactToGrabOf(block);
-                }
-                if (parent.fixLayout) parent.fixLayout();
-                parent.changed();
+        }
+        // Update parent block's UI
+        if (parent) {
+            if (parent.reactToGrabOf) {
+                parent.reactToGrabOf(block);
             }
+            if (parent.fixLayout) parent.fixLayout();
+            parent.changed();
         }
         if (scripts) {
             scripts.drawNew();
