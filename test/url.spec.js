908db3afd1bf3c3b4a51889d05822763390d1e69
Brian Broll <brian.broll@vanderbilt.edu>
2018-06-12 14:12:56 -0500

    WIP Added test for loading roles on example load

--- test/url.spec.js
+++ test/url.spec.js
@@ -3,22 +3,32 @@ describe('url anchors', function () {
     this.timeout(10000);
     // opening examples
     describe('examples', function() {
-        it('should load example using url', function () {
+        before(() => {
             const loc = {
                 href: location.origin + '?action=example&ProjectName=Dice',
                 hash: ''
             };
             return driver.reset()
-                .then(() => driver.ide().interpretUrlAnchors(loc))
-                .then(() => {
-                    return driver.expect(
-                        () => {
-                            const blockCount = driver.ide().currentSprite.scripts.children.length;
-                            return blockCount > 0;
-                        },
-                        'No blocks showed up for Battleship'
-                    );
-                });
+                .then(() => driver.ide().interpretUrlAnchors(loc));
+        });
+
+        it('should load example code', function () {
+            return driver.expect(
+                () => {
+                    const blockCount = driver.ide().currentSprite.scripts.children.length;
+                    return blockCount > 0;
+                },
+                'No blocks showed up for Battleship'
+            );
+        });
+
+        it('should load two roles (p1, p2)', function () {
+            return driver.expect(
+                () => {
+                    return driver.ide().room.getRoleCount() === 2;
+                },
+                `Found ${driver.ide().room.getRoleCount()} role(s) (expected 2)`
+            );
         });
     });
 });
