a588defb4ddf0c42d80ac1d559123708f2fd9142
TheBizzle <jason.bertsche@gmail.com>
2018-09-04 14:28:17 -0500

    Support setting variables from `postMessage`

--- on-message.js
+++ on-message.js
@@ -1,8 +1,13 @@
+window.externalVariables = {}
+
 var receiveMessage = function(event) {
   switch (event.data.type) {
     case "import":
       var nbm = world.children.find(function(x) { return x instanceof NetsBloxMorph; });
       nbm.droppedText(event.data.blocks, event.data.name);
+      break;
+    case "set-variable":
+      externalVariables[event.data.key] = event.data.value;
       break;
   }
 }
