9cfc4e412f9b0a9cf39c0704ba37428c1a4def49
Hamid Zare <dellydela@gmail.com>
2019-01-23 13:57:43 -0600

    REB snap-driver new method for creating and waiting for a role

--- test/snap-driver.js
+++ test/snap-driver.js
@@ -164,21 +164,21 @@ SnapDriver.prototype.mouseUp = function(position) {
 /**
  * Simulates mouse inputs to pick up a Morph and place it somewhere else.
  * @param {Morph} srcMorph Morph to drag and drop
- * @param {Point} position Position to drag center of srcMorph to 
+ * @param {Point} position Position to drag center of srcMorph to
  * @param {Point=} start Position to initiate drag at, if not speficied middle of left side of srcMorph is used
  */
 SnapDriver.prototype.dragAndDrop = function(srcMorph, position, start = null) {
     const {MorphicPreferences, Point} = this.globals();
-    
+
     // Drag from the upper left corner if not told otherwise
     if(start == null)
     {
         start = srcMorph.topLeft().add(new Point(2, srcMorph.height() / 2));
     }
-    
+
     // If the Morph is not grabbed at center, final position will no longer be correct
     let offset = start.subtract(srcMorph.center());
-    
+
     this.mouseDown(start);
     this.world().hand.processMouseMove({
         pageY: start.y,
@@ -249,6 +249,12 @@ SnapDriver.prototype.newRole = function(name) {
     this.keys(name);
     let dialog = this.dialog();
     dialog.ok();
+};
+
+// drives the UI to create a new role and waits for it to show up
+SnapDriver.prototype.newRoleNWait = async function(name) {
+    this.newRole(name);
+    await this.expect(() => this.ide().room.getRole(name), `new role didn't show up.`);
 };
 
 SnapDriver.prototype.moveToRole = function(name) {
