69a7ea9c759042accb873d9743126ae62e5b4b61
Brian Broll <brian.broll@gmail.com>
2019-10-15 16:23:47 -0500

    WIP save parameters in url hash

--- test/generative/main.js
+++ test/generative/main.js
@@ -2,7 +2,9 @@
 /* eslint-disable no-console, no-unused-vars */
 
 const frame = document.getElementsByTagName('iframe')[0];
-const url = window.location.href.replace(window.location.pathname, '');
+const url = window.location.href
+    .replace(window.location.pathname, '')
+    .replace(window.location.hash, '');
 frame.setAttribute('src', url);
 
 function checkLoaded() {
@@ -22,13 +24,26 @@ window.onload = () => {
 async function onIframeReady() {
     document.body.style.visibility = 'visible';
     const driver = new SnapDriver(frame.contentWindow.world);
+    const options = getOptions();
     driver.setWindow(frame.contentWindow);
-    const seed = getRandomSeed();
-    const tester = new InteractionGenerator(driver, null, seed);
-    const {SnapActions, SnapUndo, UndoManager} = driver.globals();
-    let undoCount = 0;
+    frame.style.setProperty('width', `${options.width}px`);
+    frame.style.setProperty('height', `${options.height}px`);
 
     while (true) {
+        setOptions(options);
+        await runTest(driver, options);
+
+        options.seed = Date.now();
+    }
+}
+
+async function runTest(driver, options) {
+    const {SnapActions, SnapUndo, UndoManager} = driver.globals();
+    let remainingActions = options.count;
+    let undoCount = 0;
+
+    while (remainingActions--) {
+        const tester = new InteractionGenerator(driver, null, options.seed);
         await tester.act();
         // Test that the last action can be undone (and redone)
         if (undoCount < SnapUndo.allEvents.length) {
@@ -47,14 +62,29 @@ async function onIframeReady() {
     }
 }
 
-function getRandomSeed() {
-    const querystring = window.location.href.replace(/^.*\?/, '')
-        .replace('#' + window.location.hash, '');
+function getOptions() {
+    const opts = {};
 
-    const seed = querystring.split('&')
+    window.location.hash.substring(1).split('&')
         .map(chunk => chunk.split('='))
-        .find(pair => pair[0] === 'seed');
+        .forEach(chunk => {
+            const [key, value] = chunk;
+            opts[key] = value;
+        });
 
-    return seed ? seed.pop() : null;
+    // Set defaults
+    opts.seed = opts.seed || Date.now();
+    opts.width = opts.width || document.body.getBoundingClientRect().width;
+    opts.height = opts.height || document.body.getBoundingClientRect().height;
+    opts.count = opts.count || -1;
+
+    return opts;
 }
 
+function setOptions(opts) {
+    opts.width = opts.width || frame.getBoundingClientRect().width;
+    opts.height = opts.height || frame.getBoundingClientRect().height;
+
+    window.location.hash = Object.entries(opts)
+        .map(pair => pair.join('=')).join('&');
+}
