c94c4b6e9085acb2d4c76625098fdb51c1d3b785
Brian Broll <brian.broll@gmail.com>
2019-10-16 09:15:11 -0500

    Fetch world ref from window. Closes #844

--- test/snap-driver.js
+++ test/snap-driver.js
@@ -1,5 +1,4 @@
-function SnapDriver(world) {
-    this._world = world;
+function SnapDriver() {
     this._window = window;
 }
 
@@ -13,11 +12,11 @@ SnapDriver.prototype.globals = function() {
 
 // Convenience Getters
 SnapDriver.prototype.world = function() {
-    return this._world;
+    return this._window.world;
 };
 
 SnapDriver.prototype.ide = function() {
-    return this._world.children[0];
+    return this.world().children[0];
 };
 
 SnapDriver.prototype.palette = function() {
