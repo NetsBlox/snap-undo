706360c5e05c931b7a94555fe8317d1e75775dd6
Brian Broll <brian.broll@gmail.com>
2021-03-17 12:46:21 -0500

    Add setVariable URL param. Closes #1194

--- src/gui.js
+++ src/gui.js
@@ -637,6 +637,11 @@ IDE_Morph.prototype.interpretUrlAnchors = async function (loc) {
             );
         }
     }
+
+    if (dict.setVariable) {
+        const [varName, value] = dict.setVariable.split('=');
+        await this.droppedText(value, varName, 'text');
+    }
 };
 
 // IDE_Morph construction
